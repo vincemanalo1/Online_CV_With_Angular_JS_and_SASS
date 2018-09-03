d7e6ae4afc034f917783b0aa9051b8ca1f8d7fc1
vincemanalo1 <42925251+vincemanalo1@users.noreply.github.com>
2018-09-03 19:45:44 -0300

    Add files via upload

---
+++ js/modules.js
@@ -0,0 +1,25 @@
+
+var app = angular.module('myApp', []);
+app.controller('myCtrl', function($scope) {
+    $scope.expList = [{expText:'Senior Web Developer', done:false}];
+
+    $scope.addExp = function() {
+        $scope.expList.push({expText:$scope.expInput, done:false});
+        $scope.expInput = "";
+    };
+
+    $scope.remove = function() {
+        var oldList = $scope.expList;
+        $scope.expList = [];
+        angular.forEach(oldList, function(x) {
+            if (!x.done) $scope.expList.push(x);
+        });
+    };
+
+    $scope.skills = [
+        "HTML 5",
+        "CSS 3",
+        "PHP",
+        "MySQL",
+    ]
+});
